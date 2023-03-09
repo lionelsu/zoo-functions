d31e2e516e4281784fac417a59c0c3d7da370006
lionelsu <lionel_suicune@hotmail.com>
2023-03-09 16:40:51 -0400

    req: implemented function to get ids by species

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const { species } = data;
+  return species
+    .filter((specie) => ids
+      .includes(specie.id));
 };
 
+console.log();
 module.exports = getSpeciesByIds;
