6d6d03207c213639ecda111fd8930808b105c6a1
lionelsu <lionel_suicune@hotmail.com>
2023-03-10 19:48:19 -0400

    req: implemented getSchedule function

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,38 @@
 const data = require('../data/zoo_data');
 
+const generateSchedule = () => Object.keys(data.hours)
+  .reduce((acc, day) => {
+    const officeHour = day === 'Monday'
+      ? 'CLOSED'
+      : `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+    const exhibition = day === 'Monday'
+      ? 'The zoo will be closed!'
+      : data.species
+        .filter((species) => species.availability.includes(day))
+        .map((species) => species.name);
+    acc[day] = { officeHour, exhibition };
+    return acc;
+  }, {});
+
+const getAnimals = (animalName) => {
+  const animals = data.species
+    .find((animal) => animal.name === animalName);
+  return animals ? animals.availability : null;
+};
+
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  const daysOfWeek = Object.keys(data.hours);
+
+  if (!scheduleTarget || (!getAnimals(scheduleTarget) && !daysOfWeek.includes(scheduleTarget))) {
+    return generateSchedule();
+  }
+
+  if (daysOfWeek.includes(scheduleTarget)) {
+    const daySchedule = generateSchedule()[scheduleTarget];
+    return { [scheduleTarget]: daySchedule };
+  }
+
+  return getAnimals(scheduleTarget);
 };
 
 module.exports = getSchedule;
