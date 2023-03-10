17503bad4bd1ae65f4b68522a5cfb04244f79fb9
lionelsu <lionel_suicune@hotmail.com>
2023-03-09 23:39:38 -0400

    req: implemented countAnimals and aux functions

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
+const totalAnimals = () => {
+  const animalCounts = {};
+  data.species.forEach(({ name, residents }) => {
+    animalCounts[name] = residents.length;
+  });
+  return animalCounts;
+};
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return totalAnimals();
+  }
+
+  const { species } = data;
+  const { species: animalSpecies, sex: animalSex } = animal;
+
+  const filteredSpecies = species.find(({ name }) => name === animalSpecies);
+
+  if (animalSpecies) {
+    return animalSex
+      ? filteredSpecies.residents
+        .filter(({ sex }) => sex === animalSex).length
+      : filteredSpecies.residents.length;
+  }
 };
 
 module.exports = countAnimals;
