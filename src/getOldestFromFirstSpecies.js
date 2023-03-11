1742c82146cbe910f3f325af122ff8d591eb99cb
lionelsu <lionel_suicune@hotmail.com>
2023-03-10 21:01:49 -0400

    req: implemented getOldestFromFirstSpecies function

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
+const getResposibleId = (id) => {
+  const idList = data.employees
+    .find((employee) => employee.id === id).responsibleFor;
+
+  const getId = idList
+    .find((speciesId) => data.species
+      .find((specie) => specie.id === speciesId));
+
+  return getId;
+};
+
+const getAnimalList = (id) => {
+  const animals = data.species
+    .find((specie) => specie.id === getResposibleId(id)).residents;
+
+  return animals;
+};
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const olderAnimal = getAnimalList(id)
+    .reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+
+  const oldestAnimals = Object.values(olderAnimal);
+  return oldestAnimals;
 };
 
 module.exports = getOldestFromFirstSpecies;
