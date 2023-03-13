443f6d66c34907198a6612fdaf40083dcdace998
lionelsu <lionel_suicune@hotmail.com>
2023-03-12 22:56:27 -0400

    req: implemented getAnimalMap function

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,48 @@
 const data = require('../data/zoo_data');
 
+// function filterAnimalsByLocation(animals, location) {
+//   const filteredAnimals = animals.filter(animal => animal.location === location)
+//     .map(animal => animal.name);
+//   return filteredAnimals;
+// }
+
+const animalList = () => data.species.reduce((specie, animal) => {
+  const { name, location } = animal;
+  const filteredSpecies = specie;
+
+  if (!filteredSpecies[location]) {
+    filteredSpecies[location] = [];
+  }
+
+  filteredSpecies[location].push(name);
+  return filteredSpecies;
+}, {});
+
+const getAnimalList = (sex, sorted) =>
+  data.species.reduce((specie, animal) => {
+    const { name, location, residents } = animal;
+    const filteredSpecies = specie;
+    if (!filteredSpecies[location]) {
+      filteredSpecies[location] = [];
+    }
+    let zooAnimals = residents;
+    if (sex) {
+      zooAnimals = zooAnimals
+        .filter((resident) => resident.sex === sex);
+    }
+    zooAnimals = zooAnimals.map((resident) => resident.name);
+    if (sorted) {
+      zooAnimals.sort();
+    }
+    filteredSpecies[location].push({ [name]: zooAnimals });
+    return filteredSpecies;
+  }, {});
+
 const getAnimalMap = (options) => {
-  // seu código aqui
+  if (options && options.includeNames) {
+    return getAnimalList(options.sex, options.sorted);
+  }
+  return animalList();
 };
 
 module.exports = getAnimalMap;
